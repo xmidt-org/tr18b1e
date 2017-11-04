c4c814d04e386bb4c73dbd107ee5f5be7fb9b9b0
Weston Schmidt <Weston_Schmidt@cable.comcast.com>
2017-11-03 19:06:49 -0700

    Update license details an support materials.

--- tr18b1e_test.go
+++ tr18b1e_test.go
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2016 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package tr18b1e
 
 import (
@@ -26,18 +42,18 @@ func TestPopulate(t *testing.T) {
 
 	inData := []TRData{
 		{
-			Name: "hello.world",
-			Value: "world",
+			Name:      "hello.world",
+			Value:     "world",
 			ValueType: reflect.TypeOf("world"),
 		},
 		{
-			Name: "hello.42",
-			Value: 42,
+			Name:      "hello.42",
+			Value:     42,
 			ValueType: reflect.TypeOf(42),
 		},
 		{
-			Name: "hello.stuff",
-			Value: "stuff",
+			Name:      "hello.stuff",
+			Value:     "stuff",
 			ValueType: reflect.TypeOf("stuff"),
 		},
 	}
